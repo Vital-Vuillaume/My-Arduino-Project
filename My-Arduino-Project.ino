bd7983550fd1a81f2c34f1564dfbfb78058ac609
Vital Vuillaume <vitalvuillaume@proton.me>
2023-12-10 18:58:45 +0100

    Add files via upload

--- My-Arduino-Project.ino
+++ My-Arduino-Project.ino
@@ -1,16 +1,29 @@
-const int led1 = 3;
-const int time = 2000;
+int ledRed = 3, ledYellow = 6, ledGreen = 8, comp = 7, time1 = 500;
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(led1, OUTPUT);
+  pinMode(ledRed, OUTPUT);
+  pinMode(ledYellow, OUTPUT);
+  pinMode(ledGreen, OUTPUT);
+  pinMode(comp, INPUT);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  digitalWrite(led1, HIGH);
-  delay(time);
-  digitalWrite(LED_BUILTIN, LOW);
-  digitalWrite(led1, LOW);
-  delay(time * 10);
+  if(digitalRead(comp) == HIGH) {
+    digitalWrite(ledRed, HIGH);
+    digitalWrite(ledYellow, LOW);
+    digitalWrite(ledGreen, LOW);
+    delay(time1);
+    digitalWrite(ledYellow, HIGH);
+    delay(time1);
+    digitalWrite(ledRed, LOW);
+    digitalWrite(ledYellow, LOW);
+    digitalWrite(ledGreen, HIGH);
+    delay(time1*4);
+    digitalWrite(ledYellow, HIGH);
+    digitalWrite(ledGreen, LOW);
+    delay(time1);
+    digitalWrite(ledRed, HIGH);
+    digitalWrite(ledYellow, LOW);
+    delay(time1*4);
+  }
 }
