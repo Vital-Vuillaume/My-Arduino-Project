93ce6ee4639b7df591b04e815485a0d436ebf01e
Vital Vuillaume <vitalvuillaume@proton.me>
2023-12-21 22:14:17 +0100

    Add files via upload

--- My-Arduino-Project.ino
+++ My-Arduino-Project.ino
@@ -1,43 +1,43 @@
-int ledRed = 3;
-int ledOrange = 4;
-int ledYellow = 6;
-int ledGreen = 8;
+int Red = 11;
+int Green = 10;
+int Blue = 9;
 int comp = 7;
-int blink = 100;
-int light = 1000;
-long time1 = 0;
+int time1 = 500;
+bool r = false;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(ledRed, OUTPUT);
-  pinMode(ledOrange, OUTPUT);
-  pinMode(ledYellow, OUTPUT);
-  pinMode(ledGreen, OUTPUT);
+  pinMode(Red, OUTPUT);
+  pinMode(Green, OUTPUT);
+  pinMode(Blue, OUTPUT);
   pinMode(comp, INPUT);
 }
 
 void loop() {
-  digitalWrite(ledRed, HIGH);
-  digitalWrite(ledYellow, LOW);
-  digitalWrite(ledGreen, LOW);
-  delay(blink);
-  digitalWrite(ledRed, LOW);
-  digitalWrite(ledYellow, HIGH);
-  digitalWrite(ledGreen, LOW);
-  delay(blink);
-  digitalWrite(ledRed, LOW);
-  digitalWrite(ledYellow, LOW);
-  digitalWrite(ledGreen, HIGH);
-  delay(blink);
   if(digitalRead(comp) == HIGH) {
     while(digitalRead(comp) == HIGH) {
       digitalWrite(LED_BUILTIN, HIGH);
     }
     digitalWrite(LED_BUILTIN, LOW);
-    time1 = millis();
-    digitalWrite(ledOrange, HIGH);
+    r = !r;
   }
-  if(millis() > time1 + light) {
-    digitalWrite(ledOrange, LOW);
+  if(r) {
+    digitalWrite(Red, HIGH);
+    delay(time1);  
+    digitalWrite(Green, HIGH);
+    delay(time1);
+    digitalWrite(Red, LOW);
+    delay(time1);
+    digitalWrite(Blue, HIGH);
+    delay(time1);  
+    digitalWrite(Green, LOW);
+    delay(time1);
+    digitalWrite(Red, HIGH);
+    delay(time1);
+    digitalWrite(Blue, LOW);
+  } else {
+    digitalWrite(Red, LOW);
+    digitalWrite(Green, LOW);
+    digitalWrite(Blue, LOW);
   }
 }
