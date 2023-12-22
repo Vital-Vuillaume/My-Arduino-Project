dbc8faa9013695c96b06fb4636c1ac0909e729f5
Vital Vuillaume <vitalvuillaume@proton.me>
2023-12-22 16:53:06 +0100

    Add files via upload

--- My-Arduino-Project.ino
+++ My-Arduino-Project.ino
@@ -1,15 +1,16 @@
-int Red = 11;
-int Green = 10;
-int Blue = 9;
+int red = 11;
+int green = 10;
+int blue = 9;
 int comp = 7;
-int time1 = 500;
+int time1 = 5;
+int i = 0;
 bool r = false;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(Red, OUTPUT);
-  pinMode(Green, OUTPUT);
-  pinMode(Blue, OUTPUT);
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
   pinMode(comp, INPUT);
 }
 
@@ -22,22 +23,36 @@ void loop() {
     r = !r;
   }
   if(r) {
-    digitalWrite(Red, HIGH);
-    delay(time1);  
-    digitalWrite(Green, HIGH);
-    delay(time1);
-    digitalWrite(Red, LOW);
-    delay(time1);
-    digitalWrite(Blue, HIGH);
-    delay(time1);  
-    digitalWrite(Green, LOW);
-    delay(time1);
-    digitalWrite(Red, HIGH);
-    delay(time1);
-    digitalWrite(Blue, LOW);
-  } else {
-    digitalWrite(Red, LOW);
-    digitalWrite(Green, LOW);
-    digitalWrite(Blue, LOW);
-  }
+    digitalWrite(red, HIGH);
+    digitalWrite(blue, LOW);
+    for(i=0; i <= 255; i++) {
+      analogWrite(green, i);
+      delay(time1);
+    }
+    digitalWrite(green, HIGH);
+    for(i=255; i >= 0; i--) {
+      analogWrite(red, i);
+      delay(time1);
+    }
+    digitalWrite(red, LOW);
+    for(i=0; i <= 255; i++) {
+      analogWrite(blue, i);
+      delay(time1);
+    }
+    digitalWrite(green, HIGH);
+    for(i=255; i >= 0; i--) {
+      analogWrite(green, i);
+      delay(time1);
+    }
+    digitalWrite(green, LOW);
+    for(i=0; i <= 255; i++) {
+      analogWrite(red, i);
+      delay(time1);
+    }
+    digitalWrite(red, HIGH);
+    for(i=255; i >= 0; i--) {
+      analogWrite(blue, i);
+      delay(time1);
+    }
+  } 
 }
