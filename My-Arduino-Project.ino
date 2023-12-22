76ea8815664f93f91a0a1a1b6f24381e521f194a
Vital Vuillaume <vitalvuillaume@proton.me>
2023-12-22 21:35:29 +0100

    Add files via upload

--- My-Arduino-Project.ino
+++ My-Arduino-Project.ino
@@ -1,58 +1,50 @@
+int ledRed = 3;
+int ledOrange = 4;
+int ledYellow = 6;
+int ledGreen = 8;
 int red = 11;
 int green = 10;
-int blue = 9;
 int comp = 7;
-int time1 = 5;
-int i = 0;
-bool r = false;
+int time1 = 500;
+int time2 = 2000;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(ledRed, OUTPUT);
+  pinMode(ledOrange, OUTPUT);
+  pinMode(ledYellow, OUTPUT);
+  pinMode(ledGreen, OUTPUT);
   pinMode(red, OUTPUT);
   pinMode(green, OUTPUT);
-  pinMode(blue, OUTPUT);
   pinMode(comp, INPUT);
 }
 
 void loop() {
-  if(digitalRead(comp) == HIGH) {
-    while(digitalRead(comp) == HIGH) {
-      digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(ledRed, LOW);
+    digitalWrite(ledOrange, HIGH);
+    digitalWrite(ledYellow, LOW);
+    digitalWrite(ledGreen, HIGH);
+    digitalWrite(red, HIGH);
+    digitalWrite(green, LOW);
+    if(digitalRead(comp) == HIGH) {
+      while(digitalRead(comp) == HIGH) {
+        digitalWrite(LED_BUILTIN, HIGH);
     }
     digitalWrite(LED_BUILTIN, LOW);
-    r = !r;
-  }
-  if(r) {
-    digitalWrite(red, HIGH);
-    digitalWrite(blue, LOW);
-    for(i=0; i <= 255; i++) {
-      analogWrite(green, i);
-      delay(time1);
-    }
-    digitalWrite(green, HIGH);
-    for(i=255; i >= 0; i--) {
-      analogWrite(red, i);
-      delay(time1);
-    }
+    delay(time1);
+    digitalWrite(ledYellow, HIGH);
+    digitalWrite(ledGreen, LOW);
+    delay(time1);
+    digitalWrite(ledRed, HIGH);
+    digitalWrite(ledYellow, LOW);
+    delay(time1);
     digitalWrite(red, LOW);
-    for(i=0; i <= 255; i++) {
-      analogWrite(blue, i);
-      delay(time1);
-    }
     digitalWrite(green, HIGH);
-    for(i=255; i >= 0; i--) {
-      analogWrite(green, i);
-      delay(time1);
-    }
-    digitalWrite(green, LOW);
-    for(i=0; i <= 255; i++) {
-      analogWrite(red, i);
-      delay(time1);
-    }
+    delay(time1);
     digitalWrite(red, HIGH);
-    for(i=255; i >= 0; i--) {
-      analogWrite(blue, i);
-      delay(time1);
-    }
-  } 
+    digitalWrite(green, LOW);
+    delay(time1);
+    digitalWrite(ledYellow, HIGH);
+    delay(time1);
+  }
 }
